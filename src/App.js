57f363f6a3bd215a3a66dcebb5f28b87ab52c6fe
Rafa Garrido <rafagarridolara@gmail.com>
2021-10-12 00:19:02 -0400

    Refactor ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <ExpenseItem></ExpenseItem>;
+  const expenses = [
+    {
+      id: 1,
+      title: "Car Insurance",
+      amount: 276.56,
+      date: new Date(2021, 11, 10),
+    },
+    {
+      id: 2,
+      title: "Toilet Paper",
+      amount: 74.36,
+      date: new Date(2021, 11, 10),
+    },
+    {
+      id: 3,
+      title: "New TV",
+      amount: 953.12,
+      date: new Date(2021, 11, 10),
+    },
+    {
+      id: 3,
+      title: "New Desk",
+      amount: 562.12,
+      date: new Date(2021, 11, 10),
+    },
+  ];
+
+  return (
+    <>
+      {expenses.map((expense) => (
+        <ExpenseItem {...expense} />
+      ))}
+    </>
+  );
 }
 
 export default App;
