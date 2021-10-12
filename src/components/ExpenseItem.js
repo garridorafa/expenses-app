ad66a117e073201fa4fe4e06cd01f59d3c715b64
Rafa Garrido <rafagarridolara@gmail.com>
2021-10-12 00:08:51 -0400

    Create ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 11, 10);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 276.56;
+
   return (
-    <>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className="expense-item">
+      <div>{expenseDate.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2> {expenseTitle} </h2>
+        <div className="expense-item__price"> ${expenseAmount} </div>
       </div>
-    </>
+    </div>
   );
 }
 
